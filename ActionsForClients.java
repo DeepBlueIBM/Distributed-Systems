fa6ab14b87723b75056a0021678ebffdafe6a4f9
GKotsifos <54845904+DeepBlueIBM@users.noreply.github.com>
2022-04-21 18:13:10 +0300

    Add files via upload

--- ActionsForClients.java
+++ ActionsForClients.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class ActionsForClients extends Thread{
@@ -6,5 +9,26 @@ public class ActionsForClients extends Thread{
     private String action;
     private Socket socket;
 
+    ActionsForClients(Broker broker,String action,Socket socket ){
+        this.broker = broker;
+        this.action = action;
+        this.socket=socket;
+    }
 
+    public void run() {
+
+        try {
+            InputStreamReader isr = new InputStreamReader(socket.getInputStream());
+            BufferedReader br = new BufferedReader(isr);
+            action = br.readLine();
+            action = br.readLine();
+            //stands for broker list
+            if (action.equals("bl")) {
+                broker.brokerList = broker.getOtherBrokers();
+                System.out.println("[Updated brokerList, B" + br.readLine() + " was added]");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
